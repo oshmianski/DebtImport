328565c50624481d6d00b23c5bbc9c849e82b198
VintsalovichS <oshmianski@gmail.com>
2013-10-07 17:09:17 +0300

    first release

---
+++ src/by/oshmianski/test/Address.java
@@ -0,0 +1,127 @@
+package by.oshmianski.test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: VintsalovichS
+ * Date: 07.10.13
+ * Time: 13:51
+ */
+public class Address {
+    private String index;
+    private String country;
+    private String region;
+    private String district;
+    private String unit;
+    private String cityType;
+    private String city;
+    private String streetType;
+    private String street;
+    private String house;
+    private String flat;
+
+    public Address() {
+        this("", "", "", "", "", "", "", "", "", "", "");
+    }
+
+    public Address(String index, String country, String region, String district, String unit, String cityType, String city, String streetType, String street, String house, String flat) {
+        this.index = index;
+        this.country = country;
+        this.region = region;
+        this.district = district;
+        this.unit = unit;
+        this.cityType = cityType;
+        this.city = city;
+        this.streetType = streetType;
+        this.street = street;
+        this.house = house;
+        this.flat = flat;
+    }
+
+    public String getIndex() {
+        return index;
+    }
+
+    public void setIndex(String index) {
+        this.index = index;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public String getCityType() {
+        return cityType;
+    }
+
+    public void setCityType(String cityType) {
+        this.cityType = cityType;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreetType() {
+        return streetType;
+    }
+
+    public void setStreetType(String streetType) {
+        this.streetType = streetType;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getHouse() {
+        return house;
+    }
+
+    public void setHouse(String house) {
+        this.house = house;
+    }
+
+    public String getFlat() {
+        return flat;
+    }
+
+    public void setFlat(String flat) {
+        this.flat = flat;
+    }
+}
