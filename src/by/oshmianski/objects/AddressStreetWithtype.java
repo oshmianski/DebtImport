89c96cc4c63282a31cb429aed4d55355c9e70fc9
VintsalovichS <oshmianski@gmail.com>
2013-10-18 11:15:40 +0300

    Merge branch 'master' of https://github.com/oshmianski/DebtImport
    Conflicts:
    	.idea/workspace.xml

---
+++ src/by/oshmianski/objects/AddressStreetWithtype.java
@@ -0,0 +1,31 @@
+package by.oshmianski.objects;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: VintsalovichS
+ * Date: 18.10.13
+ * Time: 9:34
+ */
+public class AddressStreetWithType {
+    private String street = "";
+    private String streetType = "";
+
+    public AddressStreetWithType() {
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getStreetType() {
+        return streetType;
+    }
+
+    public void setStreetType(String streetType) {
+        this.streetType = streetType;
+    }
+}
